c26b50a62f85c3e40081e00043f223d9b4a64fd8
Alejo-Rodri <alejandrorodriguezq0@gmail.com>
2025-11-21 23:16:59 -0500

    First commit

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 Alejandro Rodriguez alejandrorodriguezq0@gmail.com
+
+*/
+package main
+
+import "github.com/Alejo-Rodri/nebula-challenge/cmd"
+
+func main() {
+	cmd.Execute()
+}
